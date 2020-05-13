a6b4b843bf60d2a1fbee4206a5ec3b842c8f437d
tjuhel <juhel.tais@ynov.com>
2020-05-13 15:56:07 +0200

    dé

--- game.js
+++ game.js
@@ -1,4 +1,22 @@
-console.log('yo');
-
-
 plateau.style.width = "200vh";
+
+const card = ["c1", "c2", "c3", "c4", "c5", "c6", "cj"];
+const dé = document.getElementById("de") 
+const qstn = document.getElementsByClassName("qust")
+const abc = document.getElementsByClassName("abc")
+
+//Affiche la réponse
+function reponse(){
+    console.log("yooo!");
+}
+
+//Lance le dé
+function lancer(){
+    numCard = Math.floor(Math.random() * Math.floor(7));
+    const playCard = document.getElementById(card[numCard]);
+    playCard.style.transform = "translate(0%)";
+    dé.style.display= "none";
+    console.log(qstn);
+    qstn[numCard].innerHTML = "La question ?";
+    abc[numCard].innerText = "A/B/C";
+}
